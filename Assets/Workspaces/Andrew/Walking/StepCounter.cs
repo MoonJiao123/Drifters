d53a9af6dfbddca20753b4932577674f5c192f26
AndrewMJordan <andrew.m.jordan@hotmail.com>
2019-07-27 14:01:30 -0700

    Implements walking conditions

---
+++ Assets/Workspaces/Andrew/Walking/StepCounter.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace Unsorted {
+
+	public class StepCounter : MonoBehaviour {
+		public Transform target;
+
+		public float Distance;
+
+		private Vector3 lastPosition;
+
+		void Update() {
+			Vector3 delta = target.position - lastPosition;
+
+			Distance += delta.magnitude;
+
+			lastPosition = target.position;
+		}
+
+		void OnEnable() {
+			lastPosition = target.position;
+		}
+	}
+}
