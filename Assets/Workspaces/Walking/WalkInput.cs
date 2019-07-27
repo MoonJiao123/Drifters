56c932ab13bdaa0689b0dd18607b9d0290ec1f5a
AndrewMJordan <andrew.m.jordan@hotmail.com>
2019-07-27 10:59:35 -0700

    Implements lane system

---
+++ Assets/Workspaces/Walking/WalkInput.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Walking {
+
+	public class WalkInput : MonoBehaviour {
+		public WalkController controller;
+
+		#region MONOBEHAVIOUR
+		protected virtual void Update() {
+			int increment = 0;
+			if (Input.GetKeyDown(KeyCode.LeftArrow))
+				increment = -1;
+			else if (Input.GetKeyDown(KeyCode.RightArrow))
+				increment = 1;
+
+			int nextLane = controller.Lane + increment;
+			controller.Lane = nextLane;
+		}
+		#endregion MONOBEHAVIOUR
+	}
+}
