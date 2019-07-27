6d1924b907194687933e0c08aca0d019ff5b8e2b
Saoi <marie-502@hotmail.com>
2019-07-27 16:50:45 -0700

    FIXING

--- Assets/Scripts/DialogueManager.cs
+++ Assets/Scripts/DialogueManager.cs
@@ -1,18 +1,43 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class Dialogue : MonoBehaviour
+public class DialogueManager : MonoBehaviour
 {
+    public Text nameText;
+    public Text dialogueText;
+    private Queue<string> sentences;
     // Start is called before the first frame update
     void Start()
     {
-        
+        sentences = new Queue<string>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void StartDialogue(Dialogue dialogue)
     {
-        
+        nameText.text = dialogue.name;
+        sentences.Clear();
+        foreach(string s in dialogue.sentences)
+        {
+            sentences.Enqueue(s);
+        }
+        DisplayNextSentence();
+    }
+    public void DisplayNextSentence()
+    {
+        if(sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+
+        string s = sentences.Dequeue();
+        dialogueText.text = s;
+
+    }
+    void EndDialogue()
+    {
+        Debug.Log("end");
     }
 }
