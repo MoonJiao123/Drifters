8f2f0a51f2aedbaed07e38ea8e18e03ff5471fb5
AndrewMJordan <andrew.m.jordan@hotmail.com>
2019-07-27 17:21:21 -0700

    Bezier map making

---
+++ Assets/Workspaces/Andrew/PathMaker.cs
@@ -0,0 +1,43 @@
+﻿using Andtech.Bezier;
+using Andtech.Bezier.Debugging;
+using Andtech.Extrusion;
+using UnityEngine;
+
+namespace Unsorted {
+
+	[ExecuteAlways]
+	public class PathMaker : MonoBehaviour {
+		public Transform transformA;
+		public Transform transformB;
+
+		public int pointCount;
+
+		public CrossSection2D crossSection;
+
+		public MeshFilter meshFilter;
+
+		void Awake() {
+			MakeMesh();
+		}
+
+		void MakeMesh() {
+			ControlPoint cpA = new ControlPoint(transformA.position, transformA.forward * transformA.localScale.z);
+			ControlPoint cpB = new ControlPoint(transformB.position, transformB.forward * transformB.localScale.z);
+
+			Curve curve = CubicBezier.Evaluate(cpA, cpB, pointCount);
+			ExtruderResult result = Extruder.Extrude(curve, crossSection, LineTextureMode.Tile);
+
+			meshFilter.sharedMesh = result.GenerateMesh();
+		}
+
+#if UNITY_EDITOR
+		void OnValidate() {
+			MakeMesh();
+		}
+
+		void Update() {
+			MakeMesh();
+		}
+#endif
+	}
+}
