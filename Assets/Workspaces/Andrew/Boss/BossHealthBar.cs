6c79d84aa33ba9fe73fe343cd9f98c06e20d620c
AndrewMJordan <andrew.m.jordan@hotmail.com>
2019-07-28 15:13:09 -0700

    Gives the boss health

---
+++ Assets/Workspaces/Andrew/Boss/BossHealthBar.cs
@@ -0,0 +1,38 @@
+﻿using Andtech;
+using System;
+using System.Collections;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Unsorted {
+
+	public class BossHealthBar : MonoBehaviour {
+		public Image healthBar;
+		public TextMeshProUGUI textHealth;
+		public TextMeshProUGUI textHealthLabel;
+
+		void Awake() {
+			Boss.TookDamage += HandleDamage;
+		}
+
+		void OnDestroy() {
+			Boss.TookDamage -= HandleDamage;
+		}
+
+		void Start() {
+			UpdateCounters();
+		}
+
+		void UpdateCounters() {
+			healthBar.fillAmount = Boss.HealthAsPercentage;
+			textHealth.text = string.Format("{0} HP", Boss.Health);
+		}
+
+		#region CALLBACK
+		void HandleDamage(object sender, EventArgs e) {
+			UpdateCounters();
+		}
+		#endregion CALLBACK
+	}
+}
