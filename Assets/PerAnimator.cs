ef57018a062de5e2b91110ad3c7cf5c71f4814f7
AndrewMJordan <andrew.m.jordan@hotmail.com>
2019-07-27 19:48:40 -0700

    Makes the rabbit

---
+++ Assets/PerAnimator.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+namespace Unsorted {
+
+	public class PerAnimator : MonoBehaviour {
+		public Animator animator;
+
+		public Transform target;
+		public float maxSpeed;
+
+		private Vector3 lastPosition;
+
+		#region MONOBEHAVIOUR
+		void OnEnable() {
+			lastPosition = target.position;
+		}
+
+		void Update() {
+			Vector3 position = target.position;
+			Vector3 delta = position - lastPosition;
+			float distance = delta.magnitude;
+			float speed = distance / Time.deltaTime;
+			float alpha = Mathf.Clamp01(speed / maxSpeed);
+
+			animator.SetFloat("MoveSpeed", alpha);
+
+			lastPosition = position;
+		}
+        #endregion MONOBEHAVIOUR
+	}
+}
