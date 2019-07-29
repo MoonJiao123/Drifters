3eb674c1debb378ad7ce5430843000178a324a92
AndrewMJordan <andrew.m.jordan@hotmail.com>
2019-07-28 17:05:33 -0700

    Changes to portals

---
+++ Assets/OnlyAppear.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+namespace Unsorted {
+
+	public class OnlyAppear : MonoBehaviour {
+
+		#region MONOBEHAVIOUR
+		void Awake() {
+			if (GameState.HasBrain && GameState.HasCourage && GameState.HasHeart) { }
+			else
+				Destroy(gameObject);
+		}
+        #endregion MONOBEHAVIOUR
+
+        #region OVERRIDE
+        #endregion OVERRIDE
+
+        #region VIRTUAL
+        #endregion VIRTUAL
+
+        #region ABSTRACT
+        #endregion ABSTRACT
+
+        #region INTERFACE
+        #endregion INTERFACE
+
+        #region COROUTINE
+        #endregion COROUTINE
+
+        #region CALLBACK
+        #endregion CALLBACK
+
+        #region EVENT
+        #endregion EVENT
+
+        #region PIPELINE
+        #endregion PIPELINE
+	}
+}
